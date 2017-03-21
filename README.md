c3eadd096b7558ef3c80db596577fb0ba871e497
mishu <mishu@xtelecom.ro>
2017-03-21 16:35:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NullableFileds
+
+Trimmed down trait for nullable fields heavily inspired from michaeldyrynda/laravel-nullable-fields package.
+
+## Upgrade from laravel-enso v2
+
+Correct all the includes for the classes included in this package
---
+++ src/Traits/NullableFields.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaravelEnso\NullableFields\Traits;
+
+trait NullableFields
+{
+
+    abstract public function getAttributes();
+
+    protected static function bootNullableFields()
+    {
+        self::saving(function ($model) {
+            $model->setNullableFields();
+        });
+    }
+
+    protected function setNullableFields()
+    {
+        foreach ($this->nullableFromArray($this->getAttributes()) as $key => $value) {
+            $this->attributes[$key] = $this->nullIfEmpty($value);
+        }
+    }
+
+    public function nullIfEmpty($value)
+    {
+        if (is_array($value)) {
+            return empty($value) ? null : $value;
+        }
+
+        return trim($value) === '' ? null : $value;
+    }
+
+    protected function nullableFromArray(array $attributes = [])
+    {
+        if (is_array($this->nullable) && count($this->nullable) > 0) {
+            return array_intersect_key($attributes, array_flip($this->nullable));
+        }
+
+        return [];
+    }
+}
